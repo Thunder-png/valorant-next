2dc271f9e5493f479352ee2992cd36cba069a846
Arif Yıldırım <arfyldrm0606@gmail.com>
2023-05-13 14:52:45 +0300

    add style changes

--- src/pages/kontratlar.js
+++ src/pages/kontratlar.js
@@ -21,21 +21,19 @@ const KontratlarPage = () => {
             <h1 className="my-4">Kontratlar</h1>
             <div className="row">
                 <SideBar />
-                <div className="col-xl-9">
-                    <div className="row row-cols-2 row-cols-sm-3 row-cols-sm-5 g-4">
-                        {contracts.map((contract) => (
-                            <div key={contract.id} className="col">
-                                <Link href={`/kontratlar/${contract.attributes.contract_name.replace(/ /g, '-').toLowerCase()}`} className="text-decoration-none text-dark">
-                                    <div className="card h-100">
-                                        <img src={contract.attributes.contract_img} alt={contract.attributes.contract_name} className="card-img-battlepass" />
-                                        <div className="card-body">
-                                            <h3 className="card-title">{contract.attributes.contract_name}</h3>
-                                        </div>
+                <div className="row row-cols-2 row-cols-sm-3 row-cols-sm-5 g-4">
+                    {contracts.map((contract) => (
+                        <div key={contract.id} className="col">
+                            <Link href={`/kontratlar/${contract.attributes.contract_name.replace(/ /g, '-').toLowerCase()}`} className="text-decoration-none text-dark">
+                                <div className="card h-100">
+                                    <img src={contract.attributes.contract_img} alt={contract.attributes.contract_name} className="card-img-battlepass" />
+                                    <div className="card-body">
+                                        <h3 className="card-title">{contract.attributes.contract_name}</h3>
                                     </div>
-                                </Link>
-                            </div>
-                        ))}
-                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
