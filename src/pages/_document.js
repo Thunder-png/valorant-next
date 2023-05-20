2a17c01a4ce81c852dfdcc109fbb6b9a524eb633
Arif Yıldırım <arfyldrm0606@gmail.com>
2023-05-20 20:11:00 +0300

    add google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="tr">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T0VP1ZL778"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T0VP1ZL778');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
