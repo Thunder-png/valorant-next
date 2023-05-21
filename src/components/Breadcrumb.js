5e2c993604f06d3fe9fc6a07949735816c9aa629
Arif Yıldırım <arfyldrm0606@gmail.com>
2023-05-21 14:46:44 +0300

    add breadcrumb scheme

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -7,20 +7,27 @@ const Breadcrumb = () => {
     const paths = router.asPath.split("/").slice(1);
 
     return (
-        <div className="breadcrumb">
-            <Link href="/">Ana Sayfa</Link>
-            {paths.map((path, index) => {
-                const routePath = `/${paths.slice(0, index + 1).join('/')}`;
-                const isLast = index === paths.length - 1;
-                return (
-                    <span key={routePath}>
-                        {" > "}
-                        <Link href={isLast ? router.asPath : routePath}>{decodeURIComponent(path)}</Link>
-                    </span>
-                );
-            })}
-        </div>
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb small" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+                <li itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem" className="breadcrumb-item">
+                    <Link href="/" itemprop="item" title="Ana Sayfa"><span itemprop="name">Ana Sayfa</span></Link>
+                    <meta itemprop="position" content="1" />
+                </li>
+                {paths.map((path, index) => {
+                    const routePath = `/${paths.slice(0, index + 1).join('/')}`;
+                    const isLast = index === paths.length - 1;
+                    const position = index + 2;
+                    return (
+                        <li key={routePath} itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem" className={isLast ? "breadcrumb-item active text-capitalize" : "breadcrumb-item"}>
+                            <Link href={isLast ? router.asPath : routePath} itemprop="item"><span itemprop="name">{decodeURIComponent(path)}</span></Link>
+                            <meta itemprop="position" content={position} />
+                        </li>
+                    );
+                })}
+            </ol>
+        </nav>
     );
 }
 
 export default Breadcrumb;
+
