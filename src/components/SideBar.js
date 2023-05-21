7af23ff66b800a86cc3184717082d3f9ba2de630
Arif Yıldırım <arfyldrm0606@gmail.com>
2023-05-21 20:28:06 +0300

    add styles

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -31,7 +31,7 @@ const SideBar = () => {
                     <li>
                         <h5 className={styles.menuTitle}>Oyun</h5>
                         <ul className={styles.submenu}>
-                            <li><Link href="/builder"><FontAwesomeIcon icon={faGun} /> Simülatör</Link></li>
+                            <li><Link href="/builder"><FontAwesomeIcon icon={faGun} /> Simulator</Link></li>
                             <li><Link href="/ajanlar"><FontAwesomeIcon icon={faUserFriends} /> Ajanlar</Link></li>
                             <li><Link href="/silahlar"><FontAwesomeIcon icon={faCrosshairs} /> Silahlar</Link></li>
                             <li><Link href="/paketler"><FontAwesomeIcon icon={faLayerGroup} /> Koleksiyon</Link></li>
